165016a6416520187c91b30795708b0bb896bd8b
Mohamed Gamal <mgtmprog@gmail.com>
2020-10-05 21:12:17 +0200

    lcm new algorithm

--- Day 2/EASYMATH.cpp
+++ Day 2/EASYMATH.cpp
@@ -6,8 +6,18 @@ using namespace std;
 
 int DIV_BY(int number, int divBy){
     int t = number / divBy;
-    cout << number << " div by " << divBy << " " << t << endl;
+    // cout << number << " div by " << divBy << " " << t << endl;
     return t;
+}
+
+int gcd(int a, int b){
+    int temp;
+    while(b != 0){
+        temp = a;
+        a = b;
+        b = temp % b;
+    }
+    return a;
 }
 
 int lcm(int a, int b){
@@ -19,12 +29,7 @@ int lcm(int a, int b){
         min = a;
         max = b;
     }
-    int lcm = max;
-    while(true){
-        if(lcm % min == 0 && lcm % max == 0) break;
-        else lcm++;
-    }
-    return lcm;
+    return (min * max) / gcd(max, min);
 }
 
 int get_div_by(int n, int a, int d){
@@ -37,7 +42,7 @@ int get_div_by(int n, int a, int d){
     int div_by_a0a3 = DIV_BY(n, lcm(a, a+3*d));
     int div_by_a1a2 = DIV_BY(n, lcm((a+d),(a+2*d)));
     int div_by_a1a3 = DIV_BY(n, lcm((a+d), (a+3*d)));
-    int div_by_a2a3 = DIV_BY(n, lcm((a+2*d),(2+3*d)));
+    int div_by_a2a3 = DIV_BY(n, lcm((a+2*d),(a+3*d)));
     int div_by_a0a1a2 = DIV_BY(n, lcm(lcm(a, (a+d)), (a+2*d)));
     int div_by_a0a1a3 = DIV_BY(n, lcm(lcm(a, (a+d)), (a+3*d)));
     int div_by_a0a2a3 = DIV_BY(n, lcm(lcm(a, (a+2*d)), (a+3*d)));
@@ -49,7 +54,11 @@ int get_div_by(int n, int a, int d){
 }
 
 int compute(int n, int m, int a, int d){
-    return get_div_by(m, a, d) - get_div_by(n - 1, a, d);
+    int m_result = get_div_by(m, a, d);
+    int n_result = get_div_by(n - 1, a, d);
+    // cout << "numbers div by " << m << " are " << m_result << endl;
+    // cout << "numbers div by " << n << " are " << n_result << endl;
+    return m_result - n_result;
 }
 
 int main(){
@@ -58,7 +67,7 @@ int main(){
     for(int c = 0; c < number_of_inputs; c++){
         int n, m ,a, d;
         cin >> n >> m >> a >> d;
-        cout << (m - n + 1) - compute(n, m, a, d);
+        cout << m - n + 1 - compute(n, m, a, d);
     }
     return 0;
 }
